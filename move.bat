33c3d9f674e6be4b9d5d4c6e80e476344e6f1030
Captain Cobbs <egamezhq@gmail.com>
2021-04-17 04:38:48 -0600

    Revert "Dumbie"

--- move.bat
+++
@@ -1,2 +0,0 @@
-rmdir /S /Q "C:\Users\Elijah\Documents\Endless Space 2\Community\Endless Space Competitive Mod"
-Xcopy /E /I /Q "C:\Users\Elijah\source\repos\captaincobbs\ESG-Mod\Endless Space Competitive Mod" "C:\Users\Elijah\Documents\Endless Space 2\Community\Endless Space Competitive Mod"
