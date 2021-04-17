b070bb280fa6cecef94b5245e50e3410f6f06d37
Captain Cobbs <egamezhq@gmail.com>
2021-04-17 04:37:14 -0600

    Dumbie

---
+++ move.bat
@@ -0,0 +1,2 @@
+rmdir /S /Q "C:\Users\Elijah\Documents\Endless Space 2\Community\Endless Space Competitive Mod"
+Xcopy /E /I /Q "C:\Users\Elijah\source\repos\captaincobbs\ESG-Mod\Endless Space Competitive Mod" "C:\Users\Elijah\Documents\Endless Space 2\Community\Endless Space Competitive Mod"
