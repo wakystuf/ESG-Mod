ba7b9bcc5f0cf5de35e65ec3420292c80f867c19
Wakystuf <wakystuf@gmail.com>
2021-09-30 22:18:36 -0400

    Removed Provo's local folder

--- Local folder/ModMover.bat
+++
@@ -1,3 +0,0 @@
-rmdir /S /Q "C:\Users\Leo\Documents\Endless Space 2\Community\Endless Space Competitive Mod"
-Xcopy /E /I /Q C:\Users\Leo\Documents\InDevESG\ESG-Mod\"Endless Space Competitive Mod" "C:\Users\Leo\Documents\Endless Space 2\Community\Endless Space Competitive Mod"
-PAUSE
